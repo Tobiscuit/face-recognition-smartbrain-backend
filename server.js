5d4c8b118fc975eaa6fca203a90e89441b936ace
Tobiscuit <33387497+Tobiscuit@users.noreply.github.com>
2020-08-08 19:12:02 -0500

    Adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-tetrahedral-76002',
-		user: 'Toby',
-		password: '0718',
-		database: 'smart-brain'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
